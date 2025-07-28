216c441e7535230b0970a3a3a5bf173ac4fe3607
force-push-main <10535@holbertonstudents.com>
2025-07-28 12:50:36 +1000

    added code for task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""type annoted function that returns str of float"""
+import math
+
+
+def to_str(n: float) -> str:
+    """function that returns float as str"""
+
+    return str(n)
