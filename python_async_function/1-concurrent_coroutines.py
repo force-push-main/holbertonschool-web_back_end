6bf6689b15f2c31a6c8f63a40cd154e9a412c86d
force-push-main <10535@holbertonstudents.com>
2025-07-28 17:18:29 +1000

    goofy ass qu

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,11 +4,14 @@
 
 import asyncio
 import typing
-wait_random = __import__('0-basic_async_syntax.py').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> typing.Sequence[float]:
     """async func that does as stated above"""
-    funcs = [wait_random(max_delay) for _ in range(n)]
-    results = await asyncio.gather(*funcs)
+    async_funcs = [wait_random(max_delay) for _ in range(n)]
+    results = []
+    for func in asyncio.as_completed(async_funcs):
+        result = await func
+        results.append(result)
     return results
