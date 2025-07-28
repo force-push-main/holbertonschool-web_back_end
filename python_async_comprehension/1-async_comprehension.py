c2a848d12608cd7a68742208c528dd6901bf9b79
force-push-main <10535@holbertonstudents.com>
2025-07-28 18:45:55 +1000

    added code for task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,6 +6,6 @@ import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> typing.List[int]:
+async def async_comprehension() -> typing.List[float]:
     """async list comprehension"""
     return [value async for value in async_generator()]
