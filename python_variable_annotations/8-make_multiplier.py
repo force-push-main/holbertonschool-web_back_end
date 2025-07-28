351c2fb24b384ebe1a57e49552ba60bb1268b8a9
force-push-main <10535@holbertonstudents.com>
2025-07-28 13:31:24 +1000

    typing type

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -3,10 +3,9 @@
 """type annotated function that returns function"""
 
 import typing
-from collections.abc import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable[[float], float]:
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
     def callback(a_float: float):
         return a_float * multiplier
     return callback
