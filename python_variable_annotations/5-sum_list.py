169a532898b2424fbf177cab8667fb6ae6e0d02f
force-push-main <10535@holbertonstudents.com>
2025-07-28 12:58:46 +1000

    added code for task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""function with type annotations"""
+
+
+def sum_list(input_list: "list[int]") -> float:
+    """function that returns sum of list"""
+
+    return sum(input_list)
