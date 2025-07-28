0f8e7584dc5ca98d03bf8914c8e12c219fec8aed
force-push-main <10535@holbertonstudents.com>
2025-07-28 17:29:10 +1000

    not async func

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,13 +3,12 @@
 """type annotated func that returns total runtime"""
 import time
 import asyncio
-import typing
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """aforementioned annotated function"""
     start_time = time.time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
     return ((end_time - start_time) / n)
