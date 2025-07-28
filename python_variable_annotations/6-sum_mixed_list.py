630312e2b284508d1b6b0a5bb2c3d9ac4eaf31a9
force-push-main <10535@holbertonstudents.com>
2025-07-28 13:13:01 +1000

    added code for task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,5 +5,5 @@
 import typing
 
 
-def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]):
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
     return sum(mxd_lst)
