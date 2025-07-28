f66432910819ea00970361c0b6d5ecc312619fa7
force-push-main <10535@holbertonstudents.com>
2025-07-28 12:45:13 +1000

    added code for task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""type annotated function that returns concat string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """a function that returns a concat string"""
+
+    return f'{str1}{str2}'
