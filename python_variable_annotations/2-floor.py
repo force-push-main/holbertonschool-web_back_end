e7ad7db14056e46f184b636e5cd21f25b05f40e0
force-push-main <10535@holbertonstudents.com>
2025-07-28 12:48:05 +1000

    added code for task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""type annotated function that returns floor of int"""
+import math
+
+
+def floor(n: int) -> int:
+    """returns floor of int"""
+
+    return math.floor(n)
