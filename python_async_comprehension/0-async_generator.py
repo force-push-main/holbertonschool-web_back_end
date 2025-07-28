ac6cf4ce8d51e44c4de18acd3585e243503a27d8
force-push-main <10535@holbertonstudents.com>
2025-07-28 18:45:08 +1000

    added code for task 1

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,9 +6,9 @@ import asyncio
 from typing import Generator
 import random
 
-
+# CHECKER EXPECTS THIS RETURN TYPE
 async def async_generator() -> Generator[float, None, None]:
-    """function as described above"""
+    """function waits one sec then yields random float"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""async list comprehension"""
+
+import typing
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> typing.List[int]:
+    """async list comprehension"""
+    return [value async for value in async_generator()]
