cb1fef85323003dbc7254324681f2ab76006af9b
force-push-main <10535@holbertonstudents.com>
2025-07-28 17:26:06 +1000

    added code for task 2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""type annotated func that returns total runtime"""
+import time
+import asyncio
+import typing
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """aforementioned annotated function"""
+    start_time = time.time()
+    await wait_n(n, max_delay)
+    end_time = time.time()
+    return end_time - start_time
